4674d792c1ea113910c3753b91c70a9fc4577fea
Sazzad171 <sajjad.cse.diu@gmail.com>
2020-07-17 11:28:59 +0600

    third

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,8 +1,15 @@
 function showNav() {
   document.getElementById('sideNav').style.right = "0";
   document.getElementsByTagName("BODY")[0].style.overflowY = 'hidden';
+  document.getElementById('fullBodyOverlay').style.display = "block";
 }
 function hideNav() {
   document.getElementById('sideNav').style.right = "-500px";
   document.getElementsByTagName("BODY")[0].style.overflowY = 'visible';
+  document.getElementById('fullBodyOverlay').style.display = "none";
+}
+function hideOverlay() {
+  document.getElementById('sideNav').style.right = "-500px";
+  document.getElementsByTagName("BODY")[0].style.overflowY = 'visible';
+  document.getElementById('fullBodyOverlay').style.display = "none";
 }
